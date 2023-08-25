40e579f934aa9c4b37c085b57d3c2bcd745123b7
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-08-25 17:42:05 +0200

    initial structure

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import { Routes, Route } from "react-router";
+
+// pages
+import Home from './pages/Home';
+import TodoList from './pages/TodoList';
+import TodoDetails from './pages/TodoDetails';
+import TodoEdit from './pages/TodoEdit';
+import Error from './pages/Error';
+import NotFound from './pages/NotFound';
+
+// components
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todos" element={<TodoList />} />
+        <Route path="/todos/:id/details" element={<TodoDetails />} />
+        <Route path="/todos/:id/edit" element={<TodoEdit />} />
+
+        {/* error FE routes */}
+        <Route path="/error" element={<Error />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AddForm.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+function AddForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [isUrgent, setIsUrgent] = useState(false);
+
+  const handleTitleChange = (e) => setTitle(e.target.value);
+  const handleDescriptionChange = (e) => setDescription(e.target.value);
+  const handleIsUrgentChange = (e) => setIsUrgent(e.target.checked);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // ... add the ToDo here
+  }
+
+  return (
+    <div>
+      <h3>Agregar To-Do</h3>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          name="title"
+          onChange={handleTitleChange}
+          value={title}
+        />
+
+        <br />
+
+        <label htmlFor="description">Description</label>
+        <input
+          type="text"
+          name="description"
+          onChange={handleDescriptionChange}
+          value={description}
+        />
+
+        <br />
+
+        <label htmlFor="isUrgent">Urgent</label>
+        <input
+          type="checkbox"
+          name="isUrgent"
+          onChange={handleIsUrgentChange}
+          checked={isUrgent}
+        />
+
+        <br />
+
+        <button type="submit">Agregar</button>
+      
+      </form>
+    </div>
+  );
+}
+
+export default AddForm;
